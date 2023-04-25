7bd95e114cfe2e3f5af62af2004ad95e4d267a1f
BartoszWisny <261708@student.pwr.edu.pl>
2023-04-25 02:07:56 +0200

    Started creating the main view of the app

---
+++ app/src/main/java/com/example/spiral/Chat.kt
@@ -0,0 +1,8 @@
+package com.example.spiral
+
+import androidx.viewpager2.widget.ViewPager2
+
+class Chat {
+    var tabAdapter: MainActivity.TabAdapter? = null
+    var viewPager: ViewPager2? = null
+}
--- app/src/main/java/com/example/spiral/MainActivity.kt
+++ app/src/main/java/com/example/spiral/MainActivity.kt
@@ -1,18 +1,28 @@
 package com.example.spiral
 
+import android.content.res.ColorStateList
 import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.activity.OnBackPressedCallback
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.res.ResourcesCompat
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+var chat = Chat()
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mainLayout: ConstraintLayout
+    private lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mainLayout = findViewById(R.id.main_layout)
+        bottomNavigationView = findViewById(R.id.bottom_menu_bar)
 
         when (applicationContext.resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)) {
             Configuration.UI_MODE_NIGHT_YES -> {
@@ -28,5 +38,58 @@ class MainActivity : AppCompatActivity() {
                     applicationContext.theme)
             }
         }
+
+        chat.tabAdapter = TabAdapter(this)
+        chat.viewPager = findViewById(R.id.main_pager)
+        chat.viewPager?.adapter = chat.tabAdapter
+        chat.viewPager?.currentItem = 1
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (chat.viewPager?.currentItem == 1) {
+                    finish()
+                } else {
+                    chat.viewPager?.currentItem = 1
+                    chat.viewPager?.adapter = chat.tabAdapter
+                }
+            }
+        })
+    }
+
+    inner class TabAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
+        override fun getItemCount(): Int {
+            return 3
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            return when (position) {
+                0 -> {
+                    UserFriendsFragment()
+                }
+                1 -> {
+                    UserChatsFragment()
+                }
+                2 -> {
+                    UserProfileFragment()
+                }
+                else -> {
+                    UserChatsFragment()
+                }
+            }
+        }
+    }
+
+    fun onUserFriendsClick(item: MenuItem) {
+        chat.viewPager?.currentItem = 0
+        chat.viewPager?.adapter = chat.tabAdapter
+    }
+
+    fun onUserChatsClick(item: MenuItem) {
+        chat.viewPager?.currentItem = 1
+        chat.viewPager?.adapter = chat.tabAdapter
+    }
+
+    fun onUserProfileClick(item: MenuItem) {
+        chat.viewPager?.currentItem = 2
+        chat.viewPager?.adapter = chat.tabAdapter
     }
 }
