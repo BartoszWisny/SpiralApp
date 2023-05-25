2234f6fab7470e7b65da07f7141b1e2ee987c363
Swmar23 <91944872+Swmar23@users.noreply.github.com>
2023-05-25 10:49:01 +0200

    Prototype of adding profile picture from camera photo.

--- app/src/main/java/com/example/spiral/EditProfilePhotoActivity.kt
+++ app/src/main/java/com/example/spiral/EditProfilePhotoActivity.kt
@@ -5,19 +5,20 @@ import android.content.Intent
 import android.content.res.Configuration
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.FileProvider
 import androidx.core.content.res.ResourcesCompat
-import androidx.core.net.toUri
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
@@ -25,6 +26,7 @@ import com.google.firebase.storage.FirebaseStorage
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.normal.TedPermission
 import java.io.ByteArrayOutputStream
+import java.io.File
 
 class EditProfilePhotoActivity : AppCompatActivity() {
 
@@ -36,6 +38,7 @@ class EditProfilePhotoActivity : AppCompatActivity() {
     private lateinit var profilePhotoImageView: ImageView
     private lateinit var profilePhotoBitmap: Bitmap
     private lateinit var photoPath: String
+    private var photoFromCamera = false
     private var storage = FirebaseStorage.getInstance()
     private lateinit var database: DatabaseReference
 
@@ -73,12 +76,44 @@ class EditProfilePhotoActivity : AppCompatActivity() {
     }
 
     fun openCamera(view: View) {
-        Toast.makeText(this, "Not yet implemented", Toast.LENGTH_SHORT).show()
+        val permissionListener: PermissionListener = object : PermissionListener {
+            override fun onPermissionGranted() {
+                photoFromCamera = true
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                photoPath = getExternalFilesDir(Environment.DIRECTORY_PICTURES).toString() + "/photo.jpg"
+                val cameraFile = photoPath?.let { File(it) }
+                val fileUri = cameraFile?.let { FileProvider.getUriForFile(applicationContext,
+                    "com.example.spiral.file_provider", it) }
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri)
+                camera.launch(intent)
+            }
+
+            override fun onPermissionDenied(deniedPermissions: List<String>) {}
+        }
+        TedPermission.create().setScreenOrientation(resources.configuration.orientation)
+            .setPermissionListener(permissionListener).setDeniedMessage("Permission denied. If you want to take photos with"
+                    + " the camera, grant permission in the settings.").setGotoSettingButtonText("Settings")
+            .setPermissions(android.Manifest.permission.CAMERA).check()
+    }
+
+    private var camera = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            result -> if (result.resultCode == Activity.RESULT_OK) {
+                photoFromCamera = true
+                profilePhotoBitmap = BitmapFactory.decodeStream(baseContext.contentResolver.openInputStream(Uri
+                    .parse("file://$photoPath")))
+                profilePhotoImageView.setImageBitmap(profilePhotoBitmap)
+                takePhotoButton.visibility = View.GONE
+                openGalleryButton.visibility = View.GONE
+                confirmPhotoButton.visibility = View.VISIBLE
+                cancelPhotoButton.visibility = View.VISIBLE
+                profilePhotoImageView.visibility = View.VISIBLE
+            }
     }
 
     fun openGallery(view: View) {
         val permissionListener: PermissionListener = object : PermissionListener {
             override fun onPermissionGranted() {
+                photoFromCamera = false
                 val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
                 gallery.launch(intent)
             }
@@ -151,6 +186,7 @@ class EditProfilePhotoActivity : AppCompatActivity() {
 
     fun cancelPhotoClick(view: View) {
         photoPath = ""
+        photoFromCamera = false
         profilePhotoImageView.setImageResource(R.drawable.default_user_profile_photo)
         profilePhotoImageView.visibility = if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT)
             View.GONE else View.VISIBLE
@@ -163,13 +199,16 @@ class EditProfilePhotoActivity : AppCompatActivity() {
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putString("photoPath", photoPath)
+        outState.putBoolean("photoFromCamera", photoFromCamera)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         photoPath = savedInstanceState.getString("photoPath").toString()
+        photoFromCamera = savedInstanceState.getBoolean("photoFromCamera", false)
         if (photoPath != "") {
-            profilePhotoBitmap = BitmapFactory.decodeStream(baseContext.contentResolver.openInputStream(photoPath.toUri()))
+            profilePhotoBitmap = BitmapFactory.decodeStream(baseContext.contentResolver.openInputStream(Uri
+                    .parse(if (photoFromCamera) "file://$photoPath" else photoPath)))
             profilePhotoImageView.setImageBitmap(profilePhotoBitmap)
             takePhotoButton.visibility = View.GONE
             openGalleryButton.visibility = View.GONE
